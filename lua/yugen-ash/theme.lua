a33d5902111631b071da7e0e84e51cf0c4f73a71
qfioofa <penavana1@gmail.com>
2026-04-30 22:29:00 +0300

    Improved C headers | Cmd border and lua changed

--- lua/yugen-ash/theme.lua
+++ lua/yugen-ash/theme.lua
@@ -81,8 +81,8 @@ function M.get(config)
 	theme.Operator = { fg = p.frost }
 	theme.Keyword = { fg = p.crimson }
 	theme.Exception = { fg = p.tide }
-	theme.PreProc = { fg = p.color200 }
-	theme.Include = { fg = p.primary }
+	theme.PreProc = { fg = p.crimson }
+	theme.Include = { fg = p.violet }
 	theme.Type = { fg = p.gold }
 	theme.Special = { fg = p.rust }
 	theme.Tag = { fg = p.primary }
@@ -106,10 +106,6 @@ function M.get(config)
 	theme.DiagnosticVirtualTextWarn = { fg = p.warning }
 	theme.DiagnosticVirtualTextInfo = { fg = p.tide }
 	theme.DiagnosticVirtualTextHint = { fg = p.success }
-	theme.DiagnosticFloatingError = { fg = p.error, bg = groups.panel }
-	theme.DiagnosticFloatingWarn = { fg = p.warning, bg = groups.panel }
-	theme.DiagnosticFloatingInfo = { fg = p.tide, bg = groups.panel }
-	theme.DiagnosticFloatingHint = { fg = p.success, bg = groups.panel }
 	theme.DiagnosticSignError = { fg = p.error }
 	theme.DiagnosticSignWarn = { fg = p.warning }
 	theme.DiagnosticSignInfo = { fg = p.tide }
@@ -143,33 +139,98 @@ function M.get(config)
 	theme.LspDiagnosticsFloatingInformation = { fg = p.tide, bg = groups.panel }
 	theme.LspDiagnosticsFloatingHint = { fg = p.success, bg = groups.panel }
 
+	-- C specific syntax groups (matching .c and .h files)
+	theme.cTypedef = { fg = p.crimson }
+	theme.cPreCondit = { fg = p.crimson }
+	theme.cStructure = { fg = p.crimson }
+	theme.cType = { fg = p.gold }
+	theme.cStorageClass = { fg = p.crimson }
+	theme.cInclude = { fg = p.violet }
+	theme.cDefine = { fg = p.crimson }
+	theme.cMacro = { fg = p.violet }
+	theme.cSpecial = { fg = p.coral }
+	theme.cCommentString = { fg = p.sage }
+	theme.cCommentSkip = { fg = p.color400 }
+	theme.cComment2 = { fg = p.color400 }
+	theme.cConstant = { fg = p.violet }
+	theme.cInteger = { fg = p.violet }
+	theme.cFloat = { fg = p.violet }
+	theme.cString = { fg = p.sage }
+	theme.cCharacter = { fg = p.sage }
+	theme.cOperator = { fg = p.tide }
+	theme.cLabel = { fg = p.seafoam }
+	theme.cConditional = { fg = p.tide }
+	theme.cRepeat = { fg = p.tide }
+	theme.cStatement = { fg = p.color200 }
+	theme.cException = { fg = p.tide }
+	theme.cSpecialChar = { fg = p.gold }
+	theme.cSpecialComment = { fg = p.color400 }
+
 	-- Treesitter
 	theme["@variable"] = { fg = p.color200 }
+	theme["@property"] = { fg = p.color300 }
 	theme["@variable.builtin"] = { fg = p.color300 }
+	theme["@variable.parameter"] = { fg = p.color200, style = styles.italic }
+	theme["@variable.member"] = { fg = p.color500 }
+	theme["@variable.object.property"] = { fg = p.color500 }
 	theme["@constant"] = { link = "Constant" }
 	theme["@constant.builtin"] = { fg = p.violet }
+	theme["@constant.macro"] = { fg = p.violet, style = "bold" }
 	theme["@function"] = { link = "Function" }
 	theme["@function.call"] = { link = "Function" }
+	theme["@function.builtin"] = { fg = p.crimson }
+	theme["@function.macro"] = { fg = p.violet, style = "bold" }
 	theme["@keyword"] = { link = "Keyword" }
 	theme["@keyword.function"] = { fg = p.crimson }
 	theme["@keyword.import"] = { fg = p.violet }
 	theme["@keyword.operator"] = { link = "Operator" }
 	theme["@keyword.return"] = { link = "Keyword" }
+	theme["@keyword.coroutine"] = { fg = p.tide }
+	theme["@keyword.export"] = { fg = p.crimson }
 	theme["@parameter"] = { fg = p.color200 }
 	theme["@method"] = { fg = p.gold }
 	theme["@operator"] = { fg = p.tide }
 	theme["@punctuation.delimiter"] = { fg = p.color400 }
 	theme["@punctuation.bracket"] = { fg = p.color400 }
 	theme["@string"] = { link = "String" }
+	theme["@string.documentation"] = { fg = p.color400 }
+	theme["@string.regexp"] = { fg = p.coral }
+	theme["@string.escape"] = { fg = p.gold }
 	theme["@tag"] = { fg = p.sage }
 	theme["@tag.attribute"] = { fg = p.gold, style = styles.italic }
 	theme["@text"] = { fg = p.color200 }
 	theme["@text.title"] = { fg = p.primary, style = "bold" }
-	theme["@type"] = { link = "Type" }
+	theme["@type"] = { fg = p.gold }
 	theme["@type.builtin"] = { fg = p.amber }
+	theme["@type.qualifier"] = { fg = p.crimson }
+	theme["@type.definition"] = { fg = p.crimson }
 	theme["@label"] = { fg = p.seafoam }
 	theme["@conditional"] = { fg = p.tide }
 	theme["@repeat"] = { fg = p.tide }
+	theme["@storageclass"] = { fg = p.crimson }
+	theme["@structure"] = { fg = p.crimson }
+	theme["@include"] = { fg = p.violet }
+	theme["@exception"] = { fg = p.tide }
+	theme["@comment"] = { fg = p.color400 }
+	theme["@comment.error"] = { fg = p.error }
+	theme["@comment.warning"] = { fg = p.warning }
+	theme["@comment.hint"] = { fg = p.success }
+	theme["@comment.todo"] = { fg = p.gold, style = "bold" }
+
+	-- C Treesitter
+	theme["@type.c"] = { fg = p.gold }
+	theme["@type.builtin.c"] = { fg = p.gold }
+	theme["@keyword.c"] = { fg = p.tide }
+	theme["@keyword.storage.c"] = { fg = p.crimson }
+	theme["@keyword.function.c"] = { fg = p.crimson }
+	theme["@preproc.c"] = { fg = p.crimson }
+	theme["@preproc.define.c"] = { fg = p.violet, style = "bold" }
+	theme["@preproc.include.c"] = { fg = p.violet }
+	theme["@constant.c"] = { fg = p.violet }
+	theme["@macro.c"] = { fg = p.violet, style = "bold" }
+	theme["@structure.c"] = { fg = p.crimson }
+	theme["@union.c"] = { fg = p.crimson }
+	theme["@enum.c"] = { fg = p.crimson }
 
 	-- LSP
 	theme.LspReferenceText = { bg = p.color800 }
@@ -177,6 +238,9 @@ function M.get(config)
 	theme.LspReferenceWrite = { bg = p.color800 }
 	theme.LspCodeLens = { fg = p.color400 }
 	theme.LspSignatureActiveParameter = { bg = p.placeholder, style = "bold" }
+	theme.lspTypeProperty = { fg = p.color500 }
+	theme.lspTypeParameter = { fg = p.color500, style = styles.italic }
+	theme.lspTypeVariable = { fg = p.color500 }
 
 	-- Git signs
 	theme.GitSignsAdd = { fg = groups.git_add }
@@ -205,7 +269,33 @@ function M.get(config)
 	theme.TelescopeSelection = { fg = p.color200, bg = p.color600 }
 	theme.TelescopeBorder = { fg = p.color400, bg = p.color700 }
 	theme.TelescopePromptNormal = { fg = p.color200, bg = p.color700 }
+	theme.TelescopePromptBorder = { fg = p.color400, bg = p.color700 }
+	theme.TelescopePromptTitle = { fg = p.color100, bg = p.color700 }
 	theme.TelescopePreviewTitle = { fg = p.color100, bg = p.color700 }
+	theme.TelescopeResultsTitle = { fg = p.color100, bg = p.color700 }
+	theme.TelescopeMatching = { fg = p.primary }
+	theme.TelescopeSelectionCaret = { fg = p.primary }
+	theme.TelescopePromptPrefix = { fg = p.primary }
+
+	-- Noice
+	theme.NoiceCmdlineIcon = { fg = p.primary }
+	theme.NoiceCmdlineIconSearch = { fg = p.crimson }
+	theme.NoiceCmdlineIconFilter = { fg = p.crimson }
+	theme.NoiceCmdlineIconLua = { fg = p.tide }
+	theme.NoiceCmdlinePopup = { fg = p.color200, bg = p.color700 }
+	theme.NoiceCmdlinePopupBorder = { fg = p.primary }
+	theme.NoiceCmdlinePopupBorderSearch = { fg = p.crimson, bg = p.color700 }
+	theme.NoiceCmdlinePopupTitle = { fg = p.color100, bg = p.color700 }
+	theme.NoiceCmdlinePopupBorderFilter = { fg = p.crimson, bg = p.color700 }
+	theme.NoiceCmdlinePopupBorderLua = { fg = p.tide, bg = p.color700 }
+	theme.NoiceCmdlinePopupTitleSearch = { fg = p.crimson, bg = p.color700 }
+	theme.NoiceCmdlinePopupTitleFilter = { fg = p.crimson, bg = p.color700 }
+	theme.NoiceCmdlinePopupTitleLua = { fg = p.crimson, bg = p.color700 }
+	theme.NoiceFormatProgressDone = { fg = p.primary, bg = p.color700 }
+	theme.NoiceFormatProgressTodo = { fg = p.color400, bg = p.color700 }
+	theme.NoiceLspProgressClient = { fg = p.color200 }
+	theme.NoiceLspProgressPlugin = { fg = p.color400 }
+	theme.NoiceLspProgressTitle = { fg = p.primary }
 
 	-- Terminal
 	vim.g.terminal_color_0 = p.color800
